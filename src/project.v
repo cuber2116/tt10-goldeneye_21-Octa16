bda5caeac98d3bf4c4b3d77e21bba719a7b481c7
Om210 <pes2202101318@pesu.pes.edu>
2024-12-21 20:02:06 +0530

    changed a few things

--- src/project.v
+++ src/project.v
@@ -7,7 +7,7 @@
 
 
 // Integrated module for Tiny Tapeout and Octa16 processor
-module tt_um_example (
+module tt_um_octa16 (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
